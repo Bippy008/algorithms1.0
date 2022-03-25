ca1a0572d1821c9942732490740fe1a0ec91e407
Bippy008 <62377483+Bippy008@users.noreply.github.com>
2022-03-25 22:32:41 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] banknotes = {1, 3, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000};
+        int[] quantities = {10, 5, 5, 3, 2, 1, 2, 3, 3, 2, 0, 2};
+        String result = "";
+        int countOfBanknotes, totalAvailable = 0;
+
+        for (int i = 0; i < banknotes.length - 1; i++) {
+            totalAvailable += banknotes[i] * quantities[i];
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("В банкомате " + totalAvailable + "  сом.");
+        System.out.println("Введите желаемую сумму...");
+        int withdraw = scanner.nextInt();
+
+        if (totalAvailable < withdraw) {
+            System.out.println("Недостаточно средств в банкомате.");
+        } else {
+            for (int i = banknotes.length - 1; i >= 0; i--) {
+                System.out.println(0);
+                if (withdraw >= banknotes[i] && quantities[i] > 0) {
+                    countOfBanknotes = withdraw / banknotes[i];
+                    if (countOfBanknotes <= quantities[i]) {
+                        result += countOfBanknotes + "x" + banknotes[i] + " ";
+                        System.out.println(result);
+                        quantities[i] -= countOfBanknotes;
+                        totalAvailable -= countOfBanknotes * banknotes[i];
+                        withdraw -= countOfBanknotes * banknotes[i];
+                        if (totalAvailable == 0) {
+                            break;
+                        }
+                    } else {
+                        result += quantities[i] + "x" + banknotes[i];
+                        quantities[i] = 0;
+                        totalAvailable -= quantities[i] * banknotes[i];
+                    }
+
+                }
+            }
+
+            if (withdraw == 0) {
+                System.out.println("Вы получили " + result);
+            } else {
+                System.out.println("Недостаточно средств в банкомате, " +
+                        "запрашиваемая сумма не может быть выдана.");
+            }
+        }
+    }
+}
+
